ad8f042fa112f530b90dc70abb3aff9d0160fd52
sangagerard <gerrysanga33@protonmail.ch>
2021-02-07 17:56:39 +0300

    Defined the serializers and models string dunder methods

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -4,20 +4,30 @@ from core.models import *
 
 
 class TheaterSerializer(models.Model):
-    pass
+    class Meta:
+        model = Theater
+        fields = '__all__'
 
 
 class HallSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = Hall
+        fields = '__all__'
 
 
 class MovieSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = Movie
+        fields = '__all__'
 
 
 class ScreeningSerializer(models.Model):
-    pass
+    class Meta:
+        model = Screening
+        fields = '__all__'
 
 
 class TicketSerializer(models.Model):
-    pass
+    class Meta:
+        model = Ticket
+        fields = '__all__'
--- core/models.py
+++ core/models.py
@@ -6,11 +6,17 @@ class Theater(models.Model):
     name = models.CharField(max_length=50)
     contact_number = models.CharField(max_length=14)
 
+    def __str__(self):
+        return self.name
+
 
 class Hall(models.Model):
     theater = models.ForeignKey(Theater, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     number_of_seats = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.name} - {self.theater}'
 
 
 class Movie(models.Model):
@@ -20,11 +26,17 @@ class Movie(models.Model):
     duration = models.IntegerField()
     description = models.TextField()
 
+    def __str__(self):
+        return f'{self.title}'
+
 
 class Screening(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     hall = models.ForeignKey(Hall, on_delete=models.CASCADE)
     start_time = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.movie} show in {self.hall.theater} - {self.hall} at {self.start_time}'
 
 
 class Ticket(models.Model):
@@ -33,3 +45,6 @@ class Ticket(models.Model):
     seat = models.CharField(max_length=3)
     time_created = models.DateTimeField(auto_now_add=True)
     price = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.customer}\'s ticket for {self.screening}'
