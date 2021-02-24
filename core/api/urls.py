9e529aaf568afce57814fcc692e181aa77778670
sangagerard <gerrysanga33@protonmail.ch>
2021-02-24 22:45:08 +0300

    hid SECRET_KEY

--- core/api/urls.py
+++ core/api/urls.py
@@ -5,7 +5,7 @@ from .views import (NowPlayingMovies, OpeningThisWeekMovies, CategoryView,
 
 urlpatterns = [
     path('categories/', CategoryView.as_view(), name='categories'),
-    path('movie_screenings/<int:id>', MovieScreenings.as_view(), name='movie_screenings')
+    path('movie_screenings/<int:id>', MovieScreenings.as_view(), name='movie_screenings'),
     path('nearby_theaters/', NearByTheaters.as_view(), name='nearby_theaters'),
     path('now_playing_movies/', NowPlayingMovies.as_view(), name='now_playing_movies'),
     path('opening_this_week_movies/', OpeningThisWeekMovies.as_view(), name='opening_this_week_movies'),
