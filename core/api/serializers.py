770d6c5117a01286c3bd4d309a73ea8f0fdedbb5
sangagerard <gerrysanga33@protonmail.ch>
2021-03-01 16:54:48 +0300

    properly formatted how dates for screenings are to be outputed

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -30,7 +30,7 @@ class ScreeningSerializer(serializers.ModelSerializer):
     movie_name = serializers.SlugRelatedField(read_only=True, slug_field='movie')
     hall = serializers.SlugRelatedField(read_only=True, slug_field='name')
     theater = serializers.SlugRelatedField(read_only=True, slug_field='name')
-    start_time = serializers.DateTimeField(format="%d-%m-%Y %H %M")
+    start_time = serializers.DateTimeField(format="%d-%m-%Y %H:%M")
 
     class Meta:
         model = Screening
