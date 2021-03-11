f4d90d5a930e9de4a9f4cf18b589aefa599af008
sangagerard <gerrysanga33@protonmail.ch>
2021-03-11 23:37:42 +0300

    Separate views to get available seats and booking a seat
    Using get method to book a seat, POST was acting up

--- core/api/urls.py
+++ core/api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
 from .views import (NowPlayingMovies, OpeningThisWeekMovies, CategoryView,
-                    AllTheaters, MovieScreenings, BookATicketView)
+                    AllTheaters, MovieScreenings, PerformBookingView, GetFreeSeatsView,)
 
 urlpatterns = [
-    path('book_a_ticket/<int:screening_id>/<str:seat>/', BookATicketView.as_view(), name='book_a_ticket'),
-    path('get_free_seats/<int:pk>', BookATicketView.as_view(), name='get_free_seats'),
+    path('book_a_ticket/<int:screening_id>/<str:seat>/', PerformBookingView.as_view(), name='book_a_ticket'),
+    path('get_free_seats/<int:pk>', GetFreeSeatsView.as_view(), name='get_free_seats'),
     path('categories/', CategoryView.as_view(), name='categories'),
     path('movie_screenings/<int:pk>', MovieScreenings.as_view(), name='movie_screenings'),
     path('now_playing_movies/', NowPlayingMovies.as_view(), name='now_playing_movies'),
